9699f838b75535bbb700b80711752e972e4f800e
tibbs001 <sheri.tibbs@duke.edu>
2018-09-04 22:26:28 -0400

    Preliminary maps for study-related tables. (no yet tested)

---
+++ lib/vivo_mapper/resources/facility.rb
@@ -0,0 +1,13 @@
+module VivoMapper
+  class Facility < Resource
+    attr_reader :uid, :nct_id, :label
+
+    def initialize(attributes = {})
+      [ :uid, :nct_id, :label ].each do |attr|
+        instance_variable_set("@#{attr}",attributes[attr])
+      end
+      super()
+    end
+
+  end
+end
