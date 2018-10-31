6bacb2d20233f649b3abe7df0786d8e249bc62a5
tibbs001 <sheri.tibbs@duke.edu>
2018-10-31 15:44:38 -0400

    Script that successfully loads study data into mysql db

--- examples/load_study.rb
+++ examples/load_study.rb
@@ -1,24 +1,33 @@
 #require 'valium'
+studies = Aact::Study.for_vivo
+import_manager = VivoMapper::ImportManager.new(SDB_CONFIG,StdoutLogger.new(:only => [:add_to_destination,:remove_from_destination]))
+import_manager.truncate('staging')
+import_manager.simple_import('Study', studies)
+import_manager.export('Study','staging')
 
-config =  VivoMapper::Config.load_from_file('config/sdb.yml')
-im = VivoMapper::ImportManager.new(config)
-im.truncate('staging')
-
-studies = []
-rows = Aact::Study.all
-rows.each do |row|
-  row.attributes
-  studies << VivoMapper::Study.new(row)
-end
-im.simple_import('Study',studies)
-
-facilities = []
-rows = Aact::Facility.all
-rows.each do |row|
-  row.attributes
-  facilities << VivoMapper::Facility.new(row)
-end
-im.simple_import('Facility',facilities)
+import_manager = VivoMapper::ImportManager.new(SDB_CONFIG,StdoutLogger.new(:only => [:add_to_destination,:remove_from_destination]))
+import_manager.export('http://vitro.mannlib.cornell.edu/default/vitro-kb-2','destination')
 
 
-im.export('Study','staging')
+
+
+
+
+#config =  VivoMapper::Config.load_from_file('config/sdb.yml')
+#im = VivoMapper::ImportManager.new(config)
+#im.truncate('staging')
+
+#studies = Aact::Study.for_vivo
+#im.simple_import('Study',studies)
+
+#VivoMapper::ImportManager.simple_import('Study', studies)
+
+
+#facilities = []
+#rows = Aact::Facility.all
+#rows.each do |row|
+#  row.attributes
+#  facilities << VivoMapper::Facility.new(row)
+#end
+#im.simple_import('Facility',facilities)
+#im.export('Study','staging')
